5ef40575af716a7012dd01224c925978a322ab59
Reza A'masyi <mnurrreza@gmail.com>
2022-01-22 18:58:48 +0700

    feat(bindings): Add keybinding for `xkill`
    * config.def.h (keybinding): This commit add keybinding `Super + x` to activate
    `xkill` program. `xkill` is a program to forcefully kill gui program in `X11`.

--- config.def.h
+++ config.def.h
@@ -193,6 +193,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *xkillcmd[]  = { "xkill", NULL };
 
 // static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *roficmd[] = { "./.config/rofi/launchers/misc/launcher.sh", NULL };
@@ -223,6 +224,7 @@ static const char *lockcmd[] = { "dm-tools", "lock", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = xkillcmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_e,      spawn,          {.v = rofimojicmd } },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = emaevcmd } },
